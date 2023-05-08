024497ca48f785fc80e7d6d6b5c508ea509eedbe
kwasoff <iharzsnko@gmail.com>
2023-05-08 16:28:40 -0500

    Youtube music (FINAL)

--- src/test/java/tests/YoutubeMusic.java
+++ src/test/java/tests/YoutubeMusic.java
@@ -18,27 +18,27 @@ public class YoutubeMusic {
         Thread.sleep(1000);
         WebElement play = driver.findElement(By.xpath("//yt-formatted-string[@title='Unstoppable']"));
         play.click();
-        Thread.sleep(2000);
+        Thread.sleep(15000);
         WebElement nextSongButton = driver.findElement(By.xpath("//tp-yt-paper-icon-button[@class='next-button style-scope ytmusic-player-bar']"));
         nextSongButton.click();
-        Thread.sleep(2000);
+        Thread.sleep(15000);
         nextSongButton.click();
         Thread.sleep(2000);
         WebElement searchButton = driver.findElement(By.xpath("//tp-yt-paper-icon-button[@class='search-icon style-scope ytmusic-search-box']"));
         searchButton.click();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         WebElement searchBar = driver.findElement(By.xpath("//input[@class='style-scope ytmusic-search-box']"));
         searchBar.click();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         searchBar.sendKeys("Bury tomorrow - black flame (official video)");
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         searchBar.sendKeys(Keys.ARROW_DOWN);
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         searchBar.sendKeys(Keys.ENTER);
         Thread.sleep(5000);
-        WebElement playBuryTomorrow = driver.findElement(By.xpath("//div[@class='cbox yt-spec-button-shape-next--button-text-content']"));
+        WebElement playBuryTomorrow = driver.findElement(By.xpath("//button[@aria-label='PLAY ALL']"));
         playBuryTomorrow.click();
-        Thread.sleep(2000);
+        Thread.sleep(15000);
         driver.quit();
 
 
