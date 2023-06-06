4590880a2a53027dbc7ebd82d93c5f55e3d610b2
kwasoff <iharzsnko@gmail.com>
2023-06-05 19:35:36 -0500

    login/ logout

--- src/test/java/guru/AgileProject.java
+++ src/test/java/guru/AgileProject.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import utilities.ApplicationFlow;
 import utilities.Config;
@@ -14,9 +15,6 @@ public class AgileProject {
     @Test
     public void login(){
         Driver.getDriver().get(Config.getValue("guruURL"));
-        Faker faker = new Faker();
-        Driver.getDriver().findElement(By.xpath("//input[@name='emailid']")).sendKeys(faker.internet().emailAddress());
-        Driver.getDriver().findElement(By.name("btnLogin")).click();
         Driver.getDriver().findElement(By.linkText("Agile Project")).click();
         String id = Driver.getDriver().findElement(By.xpath("//ol/li[1]")).getText().substring(9);
         String pas = Driver.getDriver().findElement(By.xpath("//ol/li[2]")).getText().substring(11);
@@ -26,7 +24,30 @@ public class AgileProject {
         ApplicationFlow.pause(500);
         String expect = "https://demo.guru99.com/Agile_Project/Agi_V1/customer/Customerhomepage.php";
         Assert.assertEquals("Login functionality failed",expect,Driver.getDriver().getCurrentUrl());
+            // the message from assert pups up only when assert failed
+           }
 
+           @Test
+           public void logout(){
+               Driver.getDriver().get(Config.getValue("guruURL"));
+
+               Driver.getDriver().findElement(By.linkText("Agile Project")).click();
+               String id = Driver.getDriver().findElement(By.xpath("//ol/li[1]")).getText().substring(9);
+               String pas = Driver.getDriver().findElement(By.xpath("//ol/li[2]")).getText().substring(11);
+               Driver.getDriver().findElement(By.name("uid")).sendKeys(id);
+               Driver.getDriver().findElement(By.name("password")).sendKeys(pas);
+               Driver.getDriver().findElement(By.name("btnLogin")).click();
+               ApplicationFlow.pause(500);
+               String expect = "https://demo.guru99.com/Agile_Project/Agi_V1/customer/Customerhomepage.php";
+               Assert.assertEquals("Login functionality failed",expect,Driver.getDriver().getCurrentUrl());
+               Driver.getDriver().findElement(By.linkText("Log out")).click();
+               Alert alert = Driver.getDriver().switchTo().alert();
+               String expectedMessage = "You Have Succesfully Logged Out!!";
+               String actualMessage = alert.getText();
+               Assert.assertEquals("Messages don't match", actualMessage, expectedMessage);
+               alert.accept();
+               String expUrl = "https://demo.guru99.com/Agile_Project/Agi_V1/index.php";
+               Assert.assertEquals("URL failed", expUrl, Driver.getDriver().getCurrentUrl());
            }
 
     @After
