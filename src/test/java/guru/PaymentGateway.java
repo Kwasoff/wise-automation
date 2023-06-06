f8db32ea342fe81c23d04cd33dd07e3156fb4405
kwasoff <iharzsnko@gmail.com>
2023-06-06 12:33:02 -0500

    Page Object Model

--- src/test/java/guru/PaymentGateway.java
+++ src/test/java/guru/PaymentGateway.java
@@ -1,13 +1,17 @@
 package guru;
 
+import com.github.javafaker.Faker;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import pages.PaymentGatewayPage;
 import utilities.Config;
 import utilities.Driver;
 
-public class PaymentForElephant {
+public class PaymentGateway {
+
+    PaymentGatewayPage paymentGatewayPage = new PaymentGatewayPage();
 
     @BeforeClass
     public static void classBefore(){
@@ -27,17 +31,14 @@ public class PaymentForElephant {
 
     @Test
     public void getElephant(){
-        Driver.getDriver().findElement(By.xpath("//input")).click();
-        Driver.getDriver().findElement(By.id("card_nmuber")).sendKeys("4652550807147753");
-        WebElement month = Driver.getDriver().findElement(By.id("month"));
-        Select select1 = new Select(month);
-        select1.selectByVisibleText("08");
-        WebElement year = Driver.getDriver().findElement(By.id("year"));
-        Select select2 = new Select(year);
-        select2.selectByVisibleText("2021");
-        Driver.getDriver().findElement(By.id("cvv_code")).sendKeys("550");
-        Driver.getDriver().findElement(By.xpath("//input[@name='submit']")).click();
-        String actualResult = Driver.getDriver().findElement(By.xpath("//div[@class='table-wrapper']/h2")).getText();
+        paymentGatewayPage.buyNowBtn.click();
+        Faker faker = new Faker();
+        paymentGatewayPage.cardNumberInput.sendKeys(faker.business().creditCardNumber());
+        paymentGatewayPage.selectMonth("08");
+        paymentGatewayPage.selectYear("2021");
+        paymentGatewayPage.cvvInput.sendKeys("550");
+        paymentGatewayPage.payBtn.click();
+        String actualResult = paymentGatewayPage.paymentSuccessfullMessageGetText();
         String expected = "Payment successfull!";
 
         Assert.assertEquals(expected,actualResult);
